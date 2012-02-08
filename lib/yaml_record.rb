6ab51fa53a3b5e2e0f9936c37ee45a72353ea58f
Nathan Esquenazi <nesquena@gmail.com>
2012-02-08 03:05:56 -0800

    [yaml_record] Adds enumerable extensions

--- lib/yaml_record.rb
+++ lib/yaml_record.rb
@@ -1,6 +1,7 @@
 require 'active_support/core_ext/kernel'
 require 'active_support/core_ext/class'
 require 'active_support/core_ext/hash'
+require 'active_support/core_ext/enumerable'
 require 'active_support/secure_random'
 require 'active_support/callbacks'
 require 'yaml'
