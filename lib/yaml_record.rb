641c0fbe9f30b66be117c7e1288ec2309afd9a25
Nathan Esquenazi <nesquena@gmail.com>
2012-02-08 03:05:56 -0800

    Adds virtus as a dependency

--- lib/yaml_record.rb
+++ lib/yaml_record.rb
@@ -3,6 +3,8 @@ require 'active_support/core_ext/class'
 require 'active_support/core_ext/hash'
 require 'active_support/secure_random'
 require 'active_support/callbacks'
+require 'yaml_record/enum_ext'
+require 'virtus'
 require 'yaml'
 
 module YamlRecord
